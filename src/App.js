7cfd1cb8336913633e531f0c1e75a07fa12ef904
bhendedhawal-dhanraj <bhendedhawal.dhanraj@pnp-hcl.com>
2023-12-03 19:52:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+let timer;
 function App() {
+  const [count, setCount] = useState(0);
+  const handlClick = (event) => {
+    if (event.target.name === "start")
+      timer = setInterval(() => {
+        setCount((count) => count + 1);
+      }, 2000);
+    else if (event.target.name === "stop") clearTimeout(timer);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button name="start" onClick={handlClick}>
+          Start counting
+        </button>
+        <div>Count : {count}</div>
+        <button name="stop" onClick={handlClick}>
+          Stop counting
+        </button>
       </header>
     </div>
   );
